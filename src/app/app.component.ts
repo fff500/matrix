d1cad88d5514d7630522d9e54bfd389ea9ed5395
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2020-05-29 17:17:50 +0300

    form was added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  form: FormGroup;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      'matrixWidth': new FormControl(1, [Validators.required, Validators.min(1), Validators.max(10)]),
+      'matrixHeight': new FormControl(1, [Validators.required, Validators.min(1), Validators.max(10)]),
+    });
+  }
+
+  // dell
+  log(x) {console.log(x)};
 }
