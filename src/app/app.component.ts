1436733b719c0dc825cecfd3e6a9ab2d97cb8b00
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2020-05-29 22:27:56 +0300

    matrix drawing was added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,18 +10,74 @@ import {FormGroup, FormControl, Validators} from '@angular/forms';
 })
 export class AppComponent implements OnInit {
 
+  table: HTMLTableElement;
   form: FormGroup;
+  matrixWidth: number;
+  matrixHeight: number;
+  matrix: any[];
+  matrixIsBuilt = false;
 
   constructor() {
   }
 
   ngOnInit(): void {
+    this.table = document.querySelector('.matrix__table');
     this.form = new FormGroup({
-      'matrixWidth': new FormControl(1, [Validators.required, Validators.min(1), Validators.max(10)]),
-      'matrixHeight': new FormControl(1, [Validators.required, Validators.min(1), Validators.max(10)]),
+      'matrixWidth': new FormControl(0, [Validators.required, Validators.min(1), Validators.max(10)]),
+      'matrixHeight': new FormControl(0, [Validators.required, Validators.min(1), Validators.max(10)]),
     });
   }
 
-  // dell
-  log(x) {console.log(x)};
+  buildMatrix(): void {
+    this.matrixIsBuilt = true;
+    // let content = 0;
+    const array = [];
+    for (let i = 0; i < this.matrixHeight; i++) {
+      const temp = new Array();
+      for (let j = 0; j < this.matrixWidth; j++) {
+        temp[j] = '0';
+        // temp.push(content);
+        // content++;
+      }
+      array.push(temp);
+    }
+    this.matrix = [...array];
+    this.buildTable();
+
+
+    // del
+    console.log(this.matrix);
+  }
+
+  buildTable(): void {
+    // let counter = 0;
+    for (let x = 0; x < this.matrixHeight; x++) {
+      const tr = document.createElement('tr');
+      for (let y = 0; y < this.matrixWidth; y++) {
+        const td = document.createElement('td');
+        td.addEventListener('click', (event) => {
+          const target = event.target as HTMLTextAreaElement;
+          if (target.innerText == "0") {
+            target.style.backgroundColor = '#79d479';
+            target.innerText = '1';
+          } else {
+            target.style.backgroundColor = '#e1e1e1';
+            target.innerText = '0';
+          }
+        });
+        td.innerText = '0';
+        // td.innerText = counter.toString();
+        tr.appendChild(td);
+        // counter++;
+      }
+      this.table.appendChild(tr);
+    }
+  }
+
+  drawMatrix(): void {
+    this.table.innerHTML = '';
+    this.matrixWidth = this.form.get('matrixWidth').value;
+    this.matrixHeight = this.form.get('matrixHeight').value;
+    this.buildMatrix();
+  };
 }
